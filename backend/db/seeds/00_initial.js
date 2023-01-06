6e1f7797c994b0600119041ffa02dfbd3708efe2
RozitaTeymourzadeh <rozita.teim@gmail.com>
2023-01-06 05:33:25 +0100

    first data inserted

---
+++ backend/db/seeds/00_initial.js
@@ -0,0 +1,36 @@
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
+const Knex = require('knex'); 
+
+const orderedTableNames = require('../../src/constants/orderedTableNames');
+const tableNames = require('../../src/constants/tableNames');
+//const orderedTableNames = require('../src/constants/orderedTableNames');
+
+/**
+ * 
+ * @param {Knex} knex
+ * 
+ */
+exports.seed = async (knex) => {
+  await orderedTableNames
+    .reduce(async (promise,table_name)=>{
+      console.log('Clearing', table_name)
+      await promise;
+      return knex(table_name).del();
+    }, Promise.resolve());
+  
+  const password = crypto.randomBytes(15).toString('hex');
+  const user = {
+    email: 'rose@gmail.com',
+    name: 'Rose',
+    password: await bcrypt.hash(password, 12),
+  };
+
+  const [createdUser] = await knex(tableNames.user)
+    .insert(user)
+    .returning('*');
+
+  console.log('User created:', {
+    password,
+  }, createdUser);
+};
---
+++ backend/src/constants/orderedTableNames.js
@@ -0,0 +1,11 @@
+const tableNames = require('./tableNames');
+
+module.exports = [
+    tableNames.item,
+    tableNames.manufacturer,
+    tableNames.item_info,
+    tableNames.item_image,
+    tableNames.item_type,
+    tableNames.address,
+    tableNames.user,
+];
